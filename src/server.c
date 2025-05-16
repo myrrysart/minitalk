03c9eaa98427b800827718add55ab72b5278b554
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-16 19:39:17 +0300

    wip: catching unfinished messages seems to work, need to check more...

--- src/server.c
+++ src/server.c
@@ -6,61 +6,59 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 14:37:01 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/05/15 17:26:21 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/05/16 17:29:47 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <minitalk.h>
 
-volatile sig_atomic_t	g_state = 0;
-
-pid_t	get_client_pid(void)
-{
-	return (g_state >> 11);
-}
-
-unsigned char	get_current_byte(void)
-{
-	return (g_state & 0xFF);
-}
-
-unsigned char	get_bit_position(void)
-{
-	return ((g_state >> 8) & 0x7);
-}
-
-void	update_state(pid_t client_pid, unsigned char byte,
-		unsigned char bit_pos)
-{
-	g_state = (client_pid << 11) | (bit_pos << 8) | byte;
-}
+volatile t_message	g_message = {0};
 
 void	handle_signal(int signum, siginfo_t *info, void *context)
 {
-	unsigned char	current_byte;
-	unsigned char	bit_position;
-	pid_t			client_pid;
-
-	current_byte = get_current_byte();
-	bit_position = get_bit_position();
-	client_pid = info->si_pid;
 	(void)context;
-	if (bit_position == 0 && current_byte == 0)
-		update_state(client_pid, 0, 0);
+	if (info->si_pid <= 0)
+		return ;
+	if (g_message.client_pid != info->si_pid)
+	{
+		if (g_message.length > 0)
+			ft_printf("Incomplete message from:%d\n", g_message.client_pid);
+		g_message.client_pid = info->si_pid;
+		g_message.length = 0;
+		g_message.current_byte = 0;
+		g_message.bit_position = 0;
+		ft_printf("New client connected: %d\n", g_message.client_pid);
+	}
 	if (signum == SIGUSR1)
-		current_byte |= (1 << (7 - bit_position));
-	bit_position++;
-	if (bit_position == 8)
+		g_message.current_byte |= (1 << (7 - g_message.bit_position));
+	g_message.bit_position++;
+	if (g_message.bit_position == 8)
 	{
-		write(1, &current_byte, 1);
-		update_state(client_pid, 0, 0);
+		if (g_message.current_byte == 0)
+		{
+			if (g_message.length < MAX_MESSAGE_SIZE)
+				g_message.buffer[g_message.length] = '\0';
+			ft_printf("%s\n", g_message.buffer);
+			kill(g_message.client_pid, SIGUSR2);
+			g_message.length = 0;
+			g_message.current_byte = 0;
+			g_message.bit_position = 0;
+			return ;
+		}
+		if (g_message.length < MAX_MESSAGE_SIZE - 1)
+		{
+			g_message.buffer[g_message.length++] = g_message.current_byte;
+			g_message.buffer[g_message.length] = '\0';
+		}
+		else if (g_message.length == MAX_MESSAGE_SIZE - 1)
+		{
+			ft_printf("Message from: %d over max size\n", g_message.client_pid);
+			g_message.buffer[g_message.length] = '\0';
+		}
+		g_message.current_byte = 0;
+		g_message.bit_position = 0;
 	}
-	else
-	{
-		update_state(client_pid, current_byte, bit_position);
-	}
-	kill(client_pid, SIGUSR1);
-	if (bit_position == 8 && current_byte == 0)
-		write(1, "\n", 1);
+	kill(g_message.client_pid, SIGUSR1);
 }
 
 int	main(void)
@@ -70,11 +68,18 @@ int	main(void)
 
 	pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
+	ft_printf("Waiting for client connection...\n");
 	sa.sa_sigaction = handle_signal;
 	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
 	sa.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) ==
+		-1)
+	{
+		ft_printf("Error: Failed to set up signal handlers\n");
+		return (1);
+	}
 	while (1)
 		pause();
 	return (0);
