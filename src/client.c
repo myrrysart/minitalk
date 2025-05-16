be67de9219956aad2b3141de3ffa7fad965d9872
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-16 19:28:30 +0300

    ton of fixes
    handles sigint and sends null byte to server if it happens
    sig handler gets the server pid trough sneaky trick

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 14:37:13 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/05/15 17:34:52 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/05/16 19:15:29 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,72 @@
 
 volatile sig_atomic_t	g_state = 0;
 
-void	sig_handler(int signum)
+void	sig_handler(int signum);
+void	display_error(char *message);
+int	wait_for_handshake(void);
+int	send_byte(char c, int server_pid);
+
+int	main(int ac, char **av)
 {
-	if (signum == SIGUSR1 || signum == SIGUSR2)
-		g_state = 1;
+	int					server_pid;
+	char				*message;
+	int					i;
+	struct sigaction	sa;
+
+	if (ac != 3)
+		display_error("Usage: ./client [server PID] [message]");
+	server_pid = ft_atoi(av[1]);
+	if (server_pid <= 0)
+		display_error("Invalid PID");
+	g_state = server_pid;
+	sa.sa_handler = sig_handler;
+	sig_handler(-1);
+	g_state = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaddset(&sa.sa_mask, SIGINT);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
+	message = av[2];
+	i = 0;
+	while (message[i])
+	{
+		if (send_byte(message[i++], server_pid))
+			display_error("Server did not respond");
+	}
+	if (send_byte('\0', server_pid))
+		display_error("Server did not respond");
+	while (g_state != 2)
+		usleep(100000);
+	ft_printf("Message sent\n");
+	return (0);
 }
 
-int	display_error(char *message)
+void	sig_handler(int signum)
 {
-	while (*message)
-		write(1, message++, 1);
-	write(1, "\n", 1);
-	return (1);
+	int	server_pid;
+
+	if (signum == -1)
+		server_pid = g_state;
+	if (signum == SIGUSR1)
+		g_state = 1;
+	if (signum == SIGUSR2)
+		g_state = 2;
+	if (signum == SIGINT)
+	{
+		ft_printf("User interrupt.\n");
+		send_byte('\0', server_pid);
+		exit(130);
+	}
+}
+
+void	display_error(char *message)
+{
+	ft_printf("%s\n", message);
+	exit(1);
 }
 
 int	wait_for_handshake(void)
@@ -35,12 +89,11 @@ int	wait_for_handshake(void)
 	timeout = 0;
 	while (g_state == 0)
 	{
-		usleep(100);
+		usleep(500);
 		timeout++;
-		if (timeout > 100)
+		if (timeout > 200)
 			return (0);
 	}
-	g_state = 0;
 	return (1);
 }
 
@@ -57,36 +110,10 @@ int	send_byte(char c, int server_pid)
 			kill(server_pid, SIGUSR2);
 		if (!wait_for_handshake())
 			return (1);
+		if (g_state == 2)
+			return (0);
+		g_state = 0;
 		bit--;
 	}
-	return (0);
-}
-
-int	main(int ac, char **av)
-{
-	int					server_pid;
-	char				*message;
-	int					i;
-	struct sigaction	sa;
-
-	sa.sa_handler = sig_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	if (ac != 3)
-		return (display_error("Usage: ./client [server PID] [message]"));
-	server_pid = ft_atoi(av[1]);
-	if (server_pid <= 0)
-		return (display_error("Invalid PID"));
-	message = av[2];
-	i = 0;
-	while (message[i])
-	{
-		if (send_byte(message[i++], server_pid))
-			return (display_error("Server did not respond"));
-	}
-	if (send_byte('\0', server_pid))
-		return (display_error("Server did not respond"));
 	return (0);
 }
