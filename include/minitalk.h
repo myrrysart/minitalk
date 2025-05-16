54487495af4f4bcc8826e26756d9f43aea8c1ad4
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-16 19:37:03 +0300

    message struct for server

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:05:33 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/05/15 15:59:08 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/05/16 12:32:00 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,15 @@
 # include <signal.h>
 # include <unistd.h>
 # include <libft.h>
+# define MAX_MESSAGE_SIZE 2097152
+
+typedef struct s_message
+{
+	unsigned char	buffer[MAX_MESSAGE_SIZE];
+	size_t			length;
+	pid_t			client_pid;
+	unsigned char	current_byte;
+	unsigned char	bit_position;
+} t_message;
 
 #endif
