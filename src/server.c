e9324afcc48dc2d527edefbcbeb2e8fd2a832f0c
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-05-20 18:59:56 +0300

    done

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 14:37:01 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/05/16 17:29:47 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/05/20 18:52:00 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,61 +14,21 @@
 
 volatile t_message	g_message = {0};
 
-void	handle_signal(int signum, siginfo_t *info, void *context)
-{
-	(void)context;
-	if (info->si_pid <= 0)
-		return ;
-	if (g_message.client_pid != info->si_pid)
-	{
-		if (g_message.length > 0)
-			ft_printf("Incomplete message from:%d\n", g_message.client_pid);
-		g_message.client_pid = info->si_pid;
-		g_message.length = 0;
-		g_message.current_byte = 0;
-		g_message.bit_position = 0;
-		ft_printf("New client connected: %d\n", g_message.client_pid);
-	}
-	if (signum == SIGUSR1)
-		g_message.current_byte |= (1 << (7 - g_message.bit_position));
-	g_message.bit_position++;
-	if (g_message.bit_position == 8)
-	{
-		if (g_message.current_byte == 0)
-		{
-			if (g_message.length < MAX_MESSAGE_SIZE)
-				g_message.buffer[g_message.length] = '\0';
-			ft_printf("%s\n", g_message.buffer);
-			kill(g_message.client_pid, SIGUSR2);
-			g_message.length = 0;
-			g_message.current_byte = 0;
-			g_message.bit_position = 0;
-			return ;
-		}
-		if (g_message.length < MAX_MESSAGE_SIZE - 1)
-		{
-			g_message.buffer[g_message.length++] = g_message.current_byte;
-			g_message.buffer[g_message.length] = '\0';
-		}
-		else if (g_message.length == MAX_MESSAGE_SIZE - 1)
-		{
-			ft_printf("Message from: %d over max size\n", g_message.client_pid);
-			g_message.buffer[g_message.length] = '\0';
-		}
-		g_message.current_byte = 0;
-		g_message.bit_position = 0;
-	}
-	kill(g_message.client_pid, SIGUSR1);
-}
+static void		handle_signal(int signum, siginfo_t *info, void *context);
+static int		handle_client(siginfo_t *info);
+static void		handle_message(int signum);
 
 int	main(void)
 {
 	struct sigaction	sa;
 	pid_t				pid;
+	int					last_activity;
+	int					current_activity;
 
+	last_activity = 0;
+	current_activity = 0;
 	pid = getpid();
 	ft_printf("Server PID: %d\n", pid);
-	ft_printf("Waiting for client connection...\n");
 	sa.sa_sigaction = handle_signal;
 	sigemptyset(&sa.sa_mask);
 	sigaddset(&sa.sa_mask, SIGUSR1);
@@ -81,6 +41,77 @@ int	main(void)
 		return (1);
 	}
 	while (1)
-		pause();
+	{
+		current_activity = g_message.bit_position + g_message.length * 8;
+		if (g_message.client_pid != 0)
+		{
+			last_activity = current_activity;
+			sleep(50);
+			current_activity = g_message.bit_position + g_message.length * 8;
+			if (last_activity == current_activity)
+			{
+				ft_printf("Client timed out, server reset.");
+				g_message.length = 0;
+				g_message.client_pid = 0;
+				g_message.current_byte = 0;
+				g_message.bit_position = 0;
+			}
+		}
+		else
+			pause();
+	}
 	return (0);
+}
+
+static void	handle_signal(int signum, siginfo_t *info, void *context)
+{
+	(void)context;
+	if (info->si_pid <= 0)
+		return ;
+	if (handle_client(info))
+		return ;
+	handle_message(signum);
+}
+
+static int	handle_client(siginfo_t *info)
+{
+	if (g_message.client_pid != info->si_pid)
+	{
+		if (g_message.length > 0)
+		{
+			kill(info->si_pid, SIGUSR2);
+			return (1);
+		}
+		g_message.client_pid = info->si_pid;
+		g_message.length = 0;
+		g_message.current_byte = 0;
+		g_message.bit_position = 0;
+	}
+	return (0);
+}
+
+static void	handle_message(int signum)
+{
+	if (signum == SIGUSR1)
+		g_message.current_byte |= (1 << (7 - g_message.bit_position));
+	g_message.bit_position++;
+	if (g_message.bit_position == 8)
+	{
+		if (g_message.current_byte == 0)
+		{
+			g_message.buffer[g_message.length] = '\0';
+			ft_printf("%s\n", g_message.buffer);
+			kill(g_message.client_pid, SIGUSR2);
+			g_message.length = 0;
+			g_message.current_byte = 0;
+			g_message.bit_position = 0;
+			g_message.client_pid = 0;
+			return ;
+		}
+		g_message.buffer[g_message.length++] = g_message.current_byte;
+		g_message.buffer[g_message.length] = '\0';
+		g_message.current_byte = 0;
+		g_message.bit_position = 0;
+	}
+	kill(g_message.client_pid, SIGUSR1);
 }
